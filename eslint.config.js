1859e0ad42dbf0ac6574db942e4e4ad4a84c7d4c
Jeferson Mari <jefersonm@sprout.ph>
2024-08-28 16:57:35 +0800

    added typecsript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,24 @@
-import pluginVue from 'eslint-plugin-vue'
-export default [
-  ...pluginVue.configs['flat/recommended'],
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    },
-    parser: "vue-eslint-parser",
-  }
-]
+ import eslint from '@eslint/js';
+ import tseslint from 'typescript-eslint';
+ import pluginVue from 'eslint-plugin-vue';
+ import eslintConfigPrettier from 'eslint-config-prettier';
+
+ export default tseslint.config(
+   eslint.configs.recommended,
+   ...tseslint.configs.recommended,
+   ...pluginVue.configs['flat/recommended'],
+   {
+     plugins: {
+       'typescript-eslint': tseslint.plugin,
+     },
+     languageOptions: {
+       parserOptions: {
+         parser: '@typescript-eslint/parser',
+         project: './tsconfig.app.json',
+         extraFileExtensions: ['.vue'],
+         sourceType: 'module',
+       },
+     },
+   },
+   eslintConfigPrettier
+ );

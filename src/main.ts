911afc373022d98eda76312966fc487538c5a632
John Arias <jarias@sprout.ph>
2024-10-28 01:34:59 +0000

    Merged PR 26845: Create Side navigation Olympus Component
    Updates:
    - Create a side navigation component
    - Typography Update
    - Fix Font Weights Update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,35 @@ import { createApp } from 'vue';
 
 import App from '@/App.vue';
 
+import '@/assets/styles/fonts.css';
 import '@/assets/styles/tailwind.css';
+import 'floating-vue/dist/style.css';
 
-createApp(App).mount('#app');
+import FloatingVue from 'floating-vue';
+
+const app = createApp(App);
+
+app.use(FloatingVue, {
+  themes: {
+    'sidenav-olympus-menu': {
+      $extend: 'dropdown',
+      distance: 30,
+      placement: 'right',
+      triggers: ['hover'],
+      popperTriggers: ['hover', 'focus'],
+      autoHide: false,
+      delay: { show: 150, hide: 300 },
+    },
+    'sidenav-olympus-submenu': {
+      $extend: 'dropdown',
+      distance: 15,
+      placement: 'right',
+      triggers: ['manual'],
+      popperTriggers: ['hover', 'focus'],
+      autoHide: false,
+      delay: { show: 150, hide: 300 },
+    },
+  },
+});
+
+app.mount('#app');

2ae6ddc553dfa0a3ded491b35f7ba06796b95670
Jeferson Mari <jefersonm@sprout.ph>
2025-01-08 10:29:15 +0800

    fix eslint issue for playround

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import Inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [vue(), Inspect()],
+  plugins: [vue()],
   optimizeDeps: {
     include: ['vue', '@vue/shared'],
     exclude: ['@vue/repl'],

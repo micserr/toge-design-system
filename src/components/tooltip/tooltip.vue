1722fd8959ac5483fb1a3710f57305685f5c9f5b
John Arias <jarias@sprout.ph>
2025-02-18 07:58:55 +0000

    Merged PR 29394: Code optimization and Add HTML Support for Tooltips

--- src/components/tooltip/tooltip.vue
+++ src/components/tooltip/tooltip.vue
@@ -1,23 +1,23 @@
 <template>
-  <div v-if="$slots.default" ref="componentRef" class="relative h-min w-min whitespace-nowrap">
+  <Tooltip class="w-fit" aria-id="tooltip-wrapper" :placement="placement">
+    <template #popper>
+      <p>{{ text }}</p>
+
+      <slot name="popper-content" />
+    </template>
+
     <slot />
-    <div
-      :class="[
-        tooltipClasses,
-        'background-color-inverted absolute z-50 w-max min-w-10 max-w-72 text-wrap rounded-md px-size-spacing-4xs py-size-spacing-3xs',
-      ]"
-    >
-      <div class="text-color-inverted-strong body-xs-regular">
-        {{ text }}
-      </div>
-    </div>
-  </div>
+  </Tooltip>
 </template>
 
 <script lang="ts" setup>
+import { Tooltip } from 'floating-vue';
+import 'floating-vue/dist/style.css';
+
 import { tooltipPropTypes } from './tooltip';
 import { useTooltip } from './use-tooltip';
 
 const props = defineProps(tooltipPropTypes);
-const { componentRef, tooltipClasses, text } = useTooltip(props);
+
+const { placement } = useTooltip(props);
 </script>

a62df97fb56afe27875c801c505054417e384673
John Arias <jarias@sprout.ph>
2025-02-21 13:11:04 +0800

    Feat: Add Version Title, Checkbox Update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,34 +6,34 @@ import '@/assets/styles/tailwind.css';
 import './custom.css';
 
 const setPackageVersion = () => {
-  setInterval(() => {
-    const navbarTitle = document.querySelector('.VPNav .title span');
-    const mainText = document.querySelector('.main .text');
+  if (typeof window !== 'undefined') {
+    setInterval(() => {
+      const navbarTitle = document.querySelector('.VPNav .title span');
+      const mainText = document.querySelector('.main .text');
 
-    if (navbarTitle) {
-      const versionText = `
-        <p class="title">Sprout Design System</p>
-        <p class="version">v${pkg.version}</p>
-      `;
+      if (navbarTitle) {
+        const versionText = `
+          <p class="title">Sprout Design System</p>
+          <p class="version">v${pkg.version}</p>
+        `;
 
-      navbarTitle.innerHTML = versionText;
+        navbarTitle.innerHTML = versionText;
 
-      const version = navbarTitle.querySelector('.version');
+        const version = navbarTitle.querySelector('.version');
 
-      if (version) {
-        (version as HTMLElement).style.color = '#158039';
-        (version as HTMLElement).style.fontSize = '14px';
+        if (version) {
+          (version as HTMLElement).style.color = '#158039';
+          (version as HTMLElement).style.fontSize = '14px';
+        }
       }
-    }
 
-    if (mainText) {
-      mainText.textContent = pkg.version;
-    }
-  }, 500);
+      if (mainText) {
+        mainText.textContent = pkg.version;
+      }
+    }, 500);
+  }
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  setPackageVersion();
-});
+setPackageVersion();
 
 export default DefaultTheme;

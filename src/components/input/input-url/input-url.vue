297e53b9c4a5705baa02afe53721ee4dcda4f456
Jonahlee Xyrkcysz Fabula <jfabula@sprout.ph>
2025-02-25 14:19:29 +0800

    feat: input url

---
+++ src/components/input/input-url/input-url.vue
@@ -0,0 +1,17 @@
+<template>
+  <SprInput v-bind="$attrs" :type="'url'">
+    <template #prefix>
+      <div>
+        https://
+      </div>
+    </template>
+    <template #icon>
+      <Icon icon="ph:question-fill" />
+    </template>
+  </SprInput>
+</template>
+
+<script setup lang="ts">
+import SprInput from '@/components/input/input.vue';
+import { Icon } from "@iconify/vue";
+</script>

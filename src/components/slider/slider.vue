3ba4e97f4618b651fcdeb4c9e81551e673a1bedd
Draisdale Niño Herrera <draisdaleh@sproutmicrosoftgmail.onmicrosoft.com>
2025-03-06 05:52:28 +0000

    Merged PR 29833: FEAT SLIDER COMPONENT
    Created a slider component
    Slider documentation

---
+++ src/components/slider/slider.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    ref="sliderRef"
+    :class="[
+      handleSliderSize,
+      'spr-relative spr-w-full spr-rounded-lg spr-bg-white-100',
+      { 'spr-cursor-not-allowed spr-opacity-50': props.disabled },
+    ]"
+    role="slider"
+    :aria-valuenow="props.modelValue"
+    :aria-valuemin="props.min"
+    :aria-valuemax="props.max"
+    :aria-disabled="props.disabled"
+    tabindex="0"
+    @pointerdown="startDrag"
+  >
+    <div
+      :class="[
+        handleSliderSize,
+        'spr-absolute spr-left-0 spr-top-1/2 spr--translate-y-1/2 spr-rounded-lg spr-bg-kangkong-600',
+      ]"
+      :style="handleSliderStyle"
+    ></div>
+
+    <Icon
+      icon="ph:circle-fill"
+      :class="[
+        handleSliderThumbSize,
+        'spr-absolute spr-top-1/2 spr--translate-x-1/2 spr--translate-y-1/2 spr-transform spr-rounded-full spr-text-kangkong-600 hover:spr-text-kangkong-700',
+        { 'spr-cursor-not-allowed': props.disabled },
+      ]"
+      :style="handleThumbStyle"
+      @pointerdown="startDrag"
+    />
+  </div>
+</template>
+
+<script setup>
+import { useSlider } from './use-slider';
+import { sliderPropTypes } from './slider';
+import { Icon } from '@iconify/vue';
+
+const props = defineProps(sliderPropTypes);
+const emit = defineEmits(['update:modelValue', 'slideend']);
+
+const { sliderRef, handleSliderSize, handleSliderThumbSize, startDrag,handleSliderStyle, handleThumbStyle } = useSlider(
+  props,
+  emit,
+);
+</script>

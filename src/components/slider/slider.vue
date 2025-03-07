558c151e10eb9defca2cde15976d65fdcdf7d193
Draisdale Niño Herrera <draisdaleh@sproutmicrosoftgmail.onmicrosoft.com>
2025-03-07 00:15:50 +0000

    Merged PR 29836: [DQA] [SLIDER]
    Remove pointer events when disabled
    Adjusted css for pressed slider thumb.

--- src/components/slider/slider.vue
+++ src/components/slider/slider.vue
@@ -3,8 +3,8 @@
     ref="sliderRef"
     :class="[
       handleSliderSize,
-      'spr-relative spr-w-full spr-rounded-lg spr-bg-white-100',
-      { 'spr-cursor-not-allowed spr-opacity-50': props.disabled },
+      'spr-relative spr-w-full spr-cursor-pointer spr-rounded-lg spr-bg-white-100',
+      { 'spr-pointer-events-none spr-opacity-50': props.disabled },
     ]"
     role="slider"
     :aria-valuenow="props.modelValue"
@@ -26,8 +26,8 @@
       icon="ph:circle-fill"
       :class="[
         handleSliderThumbSize,
-        'spr-absolute spr-top-1/2 spr--translate-x-1/2 spr--translate-y-1/2 spr-transform spr-rounded-full spr-text-kangkong-600 hover:spr-text-kangkong-700',
-        { 'spr-cursor-not-allowed': props.disabled },
+        'spr-absolute spr-top-1/2 spr--translate-x-1/2 spr--translate-y-1/2 spr-transform spr-rounded-full spr-text-kangkong-600 hover:spr-text-kangkong-700 active:spr-text-kangkong-800 active:spr-border-2 active:spr-border-solid',
+        { 'spr-pointer-events-none': props.disabled },
       ]"
       :style="handleThumbStyle"
       @pointerdown="startDrag"
@@ -43,8 +43,6 @@ import { Icon } from '@iconify/vue';
 const props = defineProps(sliderPropTypes);
 const emit = defineEmits(['update:modelValue', 'slideend']);
 
-const { sliderRef, handleSliderSize, handleSliderThumbSize, startDrag,handleSliderStyle, handleThumbStyle } = useSlider(
-  props,
-  emit,
-);
+const { sliderRef, handleSliderSize, handleSliderThumbSize, startDrag, handleSliderStyle, handleThumbStyle } =
+  useSlider(props, emit);
 </script>

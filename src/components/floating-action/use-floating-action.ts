cfa43dc3a5e7871c35164ee97fc50cd0073bad64
James Manalang <jmanalang@sprout.ph>
2025-03-11 15:49:27 +0800

    chore: update use-floating props to use toRef and remove the use of classNames

--- src/components/floating-action/use-floating-action.ts
+++ src/components/floating-action/use-floating-action.ts
@@ -1,26 +1,16 @@
-import { ref, watch, computed } from 'vue';
+import { toRef, computed } from 'vue';
 import type { FloatingActionProps } from './floating-action';
-import classNames from 'classnames';
 
 export function useFloatingAction(props: FloatingActionProps) {
-  const isVisible = ref(props.show);
+  const isVisible = toRef(props, 'show');
 
   const floatingActionClasses = computed(() => {
-    const wrapperClass = classNames(
-      'spr-bg-white-50 spr-body-md-regular spr-text-color-strong spr-bg-white spr-fixed spr-bottom-8 spr-left-1/2 spr-z-50 spr-flex spr-w-full spr-max-w-[1024px] spr--translate-x-1/2 spr-items-center spr-gap-4 spr-rounded-border-radius-md  spr-drop-shadow-md',
-    );
+    const wrapperClass = 'spr-bg-white-50 spr-body-md-regular spr-text-color-strong spr-bg-white spr-fixed spr-bottom-8 spr-left-1/2 spr-z-50 spr-flex spr-w-full spr-max-w-[1024px] spr--translate-x-1/2 spr-items-center spr-gap-4 spr-rounded-border-radius-md  spr-drop-shadow-md'
 
     return {
       wrapperClass,
     };
   });
-
-  watch(
-    () => props.show,
-    (newValue) => {
-      isVisible.value = newValue;
-    },
-  );
 
   return { isVisible, floatingActionClasses };
 }

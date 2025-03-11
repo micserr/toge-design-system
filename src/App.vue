8dbf945dc63113043c21f08566bfa3791063e0a2
John Arias <jarias@sprout.ph>
2025-03-11 16:43:56 +0800

    refactor: remove unused input component and related code from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,3 @@
 <template>
   <div>Test Component Here</div>
-  <spr-input
-    v-model="inputValue"
-    label="Text Input"
-    placeholder="Enter your username"
-    :max-length="50"
-    :min-length="3"
-  />
-
-  {{ inputValue }}
 </template>
-
-<script setup>
-import { ref } from 'vue';
-import SprInput from '@/components/input/input.vue';
-
-const inputValue = ref('');
-</script>

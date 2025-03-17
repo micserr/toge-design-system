31e1cfbc12b90bfba4c45d9a6cdc8bd890f1f6a8
John Arias <jarias@sprout.ph>
2025-03-17 08:49:24 +0800

    refactor: remove unused date picker component and related code

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,3 @@
 <template>
-  <date-picker
-    id="date"
-    v-model="date"
-    display-helper
-    @get-date-formats="wew"
-    @get-input-value="wow"
-    @get-date-errors="asdjklasd"
-  />
-  {{ date }}
-
-  <br />
-  {{ sdjkfds }}
-
-  <button @click="date = '01-12-2025'">asdasd</button>
+  <div>Test Component Here</div>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-import SprRadio from '@/components/radio/radio.vue';
-import SprDatePicker from '@/components/date-picker/date-picker.vue';
-
-const date = ref('01-12-1900');
-
-const sdjkfds = ref('');
-
-const wew = (e) => {
-  console.log(e);
-};
-
-const wow = (e) => {
-  sdjkfds.value = e;
-};
-
-const asdjklasd = (e) => {
-  console.log(e);
-};
-</script>

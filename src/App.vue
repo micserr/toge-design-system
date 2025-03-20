e85e35a6e3f4822caadc9ca1bb086e772370c00a
John Arias <jarias@sprout.ph>
2025-03-20 11:08:45 +0800

    refactor: remove unused code and simplify App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
 <template>
   <div>Test Component Here</div>
-  <!-- :pre-value="adasdsadas" -->
-  <!-- Pass a static value to pre-value -->
-  <spr-input v-model="test" label="Test" :pre-value="testsss" />
-
-  {{ test }}
-  <!-- This should update dynamically as input changes -->
-
-  <button @click="test = 'Test 333333a;skd; laks;dk las;k dl;'">Change</button>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-
-import SprInput from './components/input/input.vue';
-
-const test = ref('wewew'); // The model bound to the input
-const testsss = ref('qweqweqwe');
-
-// const adasdsadas = 'asdasdlaskjdlkasjlkdjaskljdlkas'; // Static value instead of function
-</script>

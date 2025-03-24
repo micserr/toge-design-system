6cb97baa6f28bab90a5eb9b1f6bcc533d480ba0f
John Arias <jarias@sprout.ph>
2025-03-24 12:02:42 +0800

    refactor: remove contact number input component and replace with test component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,3 @@
 <template>
-  <spr-input-contact-number
-    v-model="inputValueContactNumber"
-    label="Contact Number"
-    placeholder="Enter contact number"
-    @get-selected-country-calling-code="handleSelectedCountryCallingCode"
-    @get-contact-number-errors="handleContactNumberErrors"
-  />
-  <p>Output: {{ inputValueContactNumber }}</p>
-  <p>Selected Country Code: {{ selectedCountryCode }}</p>
-  <p>Error: {{ contactNumberErrors }}</p>
+  <div>Test Component Here</div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import SprInputContactNumber from '@/components/input/input-contact-number/input-contact-number.vue';
-
-const inputValueContactNumber = ref('');
-const selectedCountryCode = ref('');
-const contactNumberErrors = ref<{ title: string; message: string }[]>([]);
-
-const handleSelectedCountryCallingCode = (value: string) => {
-  selectedCountryCode.value = value;
-};
-
-const handleContactNumberErrors = (errors: { title: string; message: string }[]) => {
-  contactNumberErrors.value = errors;
-};
-</script>

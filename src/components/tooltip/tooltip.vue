2efda4c746f1d6e6f1ff8aa9df8b70ff328f8921
John Rafael Arias <jarias@sprout.ph>
2025-03-24 05:13:26 +0000

    Merged PR 30286: Update Phone Number Input, Added Input Dropdown, Tooltip Update, Fix Using of Crypto Issue

--- src/components/tooltip/tooltip.vue
+++ src/components/tooltip/tooltip.vue
@@ -1,27 +1,36 @@
 <template>
-  <Tooltip
-    class="spr-w-fit"
-    :aria-id="props.hasMaxWidth ? 'tooltip-full-width-wrapper' : 'tooltip-wrapper'"
-    :placement="placement"
-  >
-    <template #popper>
-      <span v-if="text">{{ text }}</span>
+  <template v-if="props.text || slots['popper-content']">
+    <Tooltip
+      class="spr-w-fit"
+      :aria-id="props.hasMaxWidth ? 'tooltip-full-width-wrapper' : 'tooltip-wrapper'"
+      :placement="placement"
+    >
+      <template #popper>
+        <span v-if="props.text">{{ props.text }}</span>
 
-      <slot name="popper-content" />
-    </template>
+        <slot name="popper-content" />
+      </template>
 
+      <slot />
+    </Tooltip>
+  </template>
+  <template v-else>
     <slot />
-  </Tooltip>
+  </template>
 </template>
 
 <script lang="ts" setup>
+import { useSlots } from 'vue';
+
 import { Tooltip } from 'floating-vue';
+
 import 'floating-vue/dist/style.css';
 
 import { tooltipPropTypes } from './tooltip';
 import { useTooltip } from './use-tooltip';
 
 const props = defineProps(tooltipPropTypes);
+const slots = useSlots();
 
 const { placement } = useTooltip(props);
 </script>

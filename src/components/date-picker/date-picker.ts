10e5f4b6fdf5a6914d6b1afc4f8639e006a0c2a9
Andrea Destajo <adestajo@sprout.ph>
2025-03-27 03:33:20 +0000

    Merged PR 30454: add fullwidth prop to button and update related components/enhance date-picker placement

--- src/components/date-picker/date-picker.ts
+++ src/components/date-picker/date-picker.ts
@@ -5,6 +5,23 @@ export const definePropType = <T>(val: unknown): PropType<T> => val as PropType<
 export const REST_DAYS_TYPES = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa'] as const;
 
 const POPPER_STRATEGY_TYPES = ['fixed', 'absolute'] as const;
+const PLACEMENTS_TYPES = [
+  'auto',
+  'auto-start',
+  'auto-end',
+  'top',
+  'top-start',
+  'top-end',
+  'right',
+  'right-start',
+  'right-end',
+  'bottom',
+  'bottom-start',
+  'bottom-end',
+  'left',
+  'left-start',
+  'left-end',
+] as const;
 
 export const datePickerPropTypes = {
   id: {
@@ -77,6 +94,11 @@ export const datePickerPropTypes = {
     type: String,
     validator: (value: 'fixed' | 'absolute') => POPPER_STRATEGY_TYPES.includes(value),
     default: 'absolute',
+  },
+  placement: {
+    type: String as PropType<(typeof PLACEMENTS_TYPES)[number]>,
+    validator: (value: (typeof PLACEMENTS_TYPES)[number]) => PLACEMENTS_TYPES.includes(value),
+    default: 'bottom',
   },
 };
 

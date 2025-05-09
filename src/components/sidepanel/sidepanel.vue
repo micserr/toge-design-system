6a5929c1b9244c172b7c583dd4e70a125bced3b0
John Rafael Arias <jarias@sprout.ph>
2025-05-09 05:30:40 +0000

    Merged PR 31418: Update mobile responsiveness sidepanel

--- src/components/sidepanel/sidepanel.vue
+++ src/components/sidepanel/sidepanel.vue
@@ -1,16 +1,13 @@
 <template>
-  <div
-    v-if="isOpen && hasBackdrop"
-    class="spr-fixed spr-left-0 spr-top-0 spr-z-[1010] spr-h-screen spr-w-screen spr-bg-mushroom-700/60"
-  ></div>
+  <div v-if="isOpen && hasBackdrop" :class="sidepanelClasses.backdropBaseClasses"></div>
+
   <Transition
-    name="sidepanel"
-    enter-active-class="spr-transition-transform spr-duration-[150ms] spr-ease-[ease-in-out]"
-    leave-active-class="spr-transition-transform spr-duration-[150ms] spr-ease-[ease-in-out]"
-    :enter-from-class="sidepanelStartEndState"
-    :leave-to-class="sidepanelStartEndState"
-    :enter-to-class="sidepanelMidState"
-    :leave-from-class="sidepanelMidState"
+    :enter-active-class="sidepanelClasses.sidepanelTransitionActiveClasses"
+    :leave-active-class="sidepanelClasses.sidepanelTransitionActiveClasses"
+    :enter-from-class="sidepanelClasses.sidepanelTransitionHiddenClasses"
+    :enter-to-class="sidepanelClasses.sidepanelTransitionVisibleClasses"
+    :leave-from-class="sidepanelClasses.sidepanelTransitionVisibleClasses"
+    :leave-to-class="sidepanelClasses.sidepanelTransitionHiddenClasses"
     appear
   >
     <div
@@ -19,33 +16,26 @@
       role="dialog"
       aria-labelledby="sidepanel-title"
       aria-describedby="sidepanel-content"
-      :class="[
-        sidepanelSizesClasses,
-        'spr-fixed spr-right-4 spr-top-1/2 spr-z-[1015] spr-flex spr-h-full spr-min-h-[200px] spr-translate-y-[-50%] spr-flex-col spr-rounded-border-radius-xl spr-bg-white-50 spr-drop-shadow',
-      ]"
+      :class="sidepanelClasses.sidepanelBaseClasses"
       :style="{ height: typeof height === 'number' ? `${height}px` : height }"
     >
-      <template v-if="!hideHeader">
-        <div
-          v-if="!$slots.header"
-          class="spr-tw-min-h-12 spr-text-color-strong spr-flex spr-justify-between spr-border-0 spr-border-b spr-border-solid spr-border-mushroom-200 spr-p-4"
-        >
-          <div id="sidepanel-title" class="spr-subheading-xs">
+      <template v-if="!props.hideHeader">
+        <div v-if="!$slots.header" :class="sidepanelClasses.sidepanelHeaderClasses">
+          <div id="sidepanel-title" :class="sidepanelClasses.sidepanelHeaderTitleClasses">
             {{ headerTitle }}
           </div>
-          <Icon class="spr-text-color-weak spr-h-5 spr-w-5 spr-cursor-pointer" icon="ph:x" @click="handleClose" />
+          <Icon :class="sidepanelClasses.sidepanelHeaderIconClasses" icon="ph:x" @click="handleClose" />
         </div>
         <div v-else>
           <slot name="header"></slot>
         </div>
       </template>
-      <div id="sidepanel-content" :class="['spr-h-full spr-overflow-y-auto']">
+
+      <div id="sidepanel-content" :class="sidepanelClasses.sidepanelContentClasses">
         <slot />
       </div>
-      <div
-        v-if="$slots.footer"
-        class="spr-bottom-0 spr-left-0 spr-w-full spr-rounded-b-border-radius-xl spr-border-0 spr-border-t spr-border-solid spr-border-mushroom-200 spr-bg-white-50 spr-py-3"
-      >
+
+      <div v-if="$slots.footer" :class="sidepanelClasses.sidepanelFooterClasses">
         <slot name="footer"></slot>
       </div>
     </div>
@@ -61,8 +51,5 @@ import { sidepanelPropTypes, sidepanelEmitTypes } from './sidepanel';
 const props = defineProps(sidepanelPropTypes);
 const emit = defineEmits(sidepanelEmitTypes);
 
-const { sidepanelRef, sidepanelSizesClasses, sidepanelMidState, sidepanelStartEndState, handleClose } = useSidepanel(
-  props,
-  emit,
-);
+const { sidepanelClasses, sidepanelRef, handleClose } = useSidepanel(props, emit);
 </script>

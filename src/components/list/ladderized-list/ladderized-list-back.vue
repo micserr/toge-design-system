d4f33f3a1e26fe76d75a85d23ec3ba8dec2b92fe
John Arias <jarias@sprout.ph>
2025-05-28 16:13:08 +0800

    refactor: improve code formatting and consistency in ladderized list components

--- src/components/list/ladderized-list/ladderized-list-back.vue
+++ src/components/list/ladderized-list/ladderized-list-back.vue
@@ -1,12 +1,10 @@
 <template>
   <div
-    class="
-    spr-py-size-spacing-2xs spr-px-size-spacing-xs 
-    spr-body-xs-regular 
-    spr-flex spr-items-center spr-gap-size-spacing-5xs
-    spr-border-solid spr-border-0 spr-border-b spr-border-color-weak 
-    spr-cursor-pointer
-    "
+    :class="[
+      'spr-flex spr-cursor-pointer spr-items-center spr-gap-size-spacing-5xs spr-px-size-spacing-xs spr-py-size-spacing-2xs',
+      'spr-body-xs-regular',
+      'spr-border-color-weak spr-border-0 spr-border-b spr-border-solid',
+    ]"
     @click="handleBackClick"
   >
     <Icon icon="ph:arrow-left" class="spr-size-4" />
@@ -20,15 +18,15 @@ import { Icon } from '@iconify/vue';
 const props = defineProps({
   label: {
     type: String,
-    default: "Back"
+    default: 'Back',
   },
 });
 
 const emit = defineEmits({
-  back: () => true
+  back: () => true,
 });
 
 const handleBackClick = () => {
-  emit("back");
+  emit('back');
 };
 </script>

5309fe1251788632ea1ab011a2cbfb122b87c6be
John Arias <jarias@sprout.ph>
2025-05-28 11:38:05 +0800

    refactor: improve dropdown and list component logic for better readability and performance

--- src/components/dropdown/dropdown.vue
+++ src/components/dropdown/dropdown.vue
@@ -1,4 +1,5 @@
 <template>
+  {{ dropdownValue }}
   <Menu
     :shown="dropdownPopperState"
     aria-id="dropdown-wrapper"
@@ -68,11 +69,16 @@
 
 <script lang="ts" setup>
 import { Menu } from 'floating-vue';
+
 import 'floating-vue/dist/style.css';
-import { dropdownPropTypes, dropdownEmitTypes } from './dropdown';
-import { useDropdown } from './use-dropdown';
+
 import SprList from '../list/list.vue';
 import SprLadderizedList from '../list/ladderized-list/ladderized-list.vue';
+
+import { dropdownPropTypes, dropdownEmitTypes } from './dropdown';
+
+import { useDropdown } from './use-dropdown';
+
 const props = defineProps(dropdownPropTypes);
 const emit = defineEmits(dropdownEmitTypes);
 

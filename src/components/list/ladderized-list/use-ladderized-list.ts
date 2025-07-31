643370af6bad92ced9066f4dbab9da066fd40383
John Arias <jarias@sprout.ph>
2025-07-31 20:46:45 +0800

    refactor: change filterOptionsRecursive to an arrow function and improve search logic

--- src/components/list/ladderized-list/use-ladderized-list.ts
+++ src/components/list/ladderized-list/use-ladderized-list.ts
@@ -20,27 +20,35 @@ export const useLadderizedList = (
   const activeLevel = ref(0);
   const activeList = ref<MenuListType[]>(menuList.value); // List of items to display in the active level
   const searchText = ref('');
+
   // Recursive filter function for ladderized options
-  function filterOptionsRecursive(items: MenuListType[], search: string): MenuListType[] {
+  const filterOptionsRecursive = (items: MenuListType[], search: string): MenuListType[] => {
     if (!search) return items;
+
     const lowerSearch = search.toLowerCase();
+
     return items
       .map((item) => {
         let match =
           item.text.toLowerCase().includes(lowerSearch) ||
           (item.subtext && item.subtext.toLowerCase().includes(lowerSearch));
+
         const filteredSublevel = item.sublevel ? filterOptionsRecursive(item.sublevel, search) : undefined;
+
         if (filteredSublevel && filteredSublevel.length > 0) match = true;
+
         if (match) {
           return {
             ...item,
             sublevel: filteredSublevel,
           };
         }
+
         return null;
       })
       .filter(Boolean) as MenuListType[];
-  }
+  };
+
   // Watch for searchText changes and update activeList
   watch(searchText, (val) => {
     if (val) {
@@ -49,6 +57,7 @@ export const useLadderizedList = (
       activeList.value = menuList.value;
     }
   });
+
   const prevList = ref<MenuListType[]>([]);
 
   const handleSelectedListItem = (item: MenuListType) => {

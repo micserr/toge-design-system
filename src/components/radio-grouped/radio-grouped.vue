0c09ebf457e733954f22246a0eb0f0f73dbc52a1
John Arias <jarias@sprout.ph>
2025-11-19 21:31:17 +0800

    refactor: update display helper implementation in Radio Grouped component for improved clarity and functionality

--- src/components/radio-grouped/radio-grouped.vue
+++ src/components/radio-grouped/radio-grouped.vue
@@ -14,13 +14,11 @@
       </spr-radio>
     </div>
 
-    <div v-if="(displayHelper && props.helperText) || error" :class="radioGroupedClasses.helperClasses">
-      <Icon
-        v-if="error || props.helperIcon"
-        class="spr-h-5 spr-min-h-5 spr-w-5 spr-min-w-5"
-        :icon="error ? 'ph:info-fill' : props.helperIcon || ''"
-      />
-      <span>{{ props.helperText }}</span>
+    <div v-if="props.displayHelper" :class="radioGroupedClasses.helperClasses">
+      <slot name="helperMessage">
+        <Icon v-if="props.helperIcon" class="spr-h-5 spr-min-h-5 spr-w-5 spr-min-w-5" :icon="props.helperIcon" />
+        <span>{{ props.helperText }}</span>
+      </slot>
     </div>
   </div>
 </template>
@@ -35,8 +33,5 @@ import { useRadioGrouped } from './use-radio-grouped';
 const props = defineProps(radioGroupedPropTypes);
 const emit = defineEmits(radioGroupedEmitTypes);
 
-const { radioGroupedClasses, proxyValue, renderOptions, isOptionDisabled, error, displayHelper } = useRadioGrouped(
-  props,
-  emit,
-);
+const { radioGroupedClasses, proxyValue, renderOptions, isOptionDisabled } = useRadioGrouped(props, emit);
 </script>
